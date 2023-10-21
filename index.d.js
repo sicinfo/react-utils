d2b52474a048c98d7855fa5373846fcdc4995bc6
moreira <moreiraxx+github-reactjs@gmail.com>
2023-10-21 12:40:53 -0300

    initial

---
+++ index.d.js
@@ -0,0 +1,16 @@
+
+/*
+ * @typedef {React.createElement} ReactCreateElement
+ * @typedef {React.useState} ReactUseState
+ * @typedef {React.Suspense} ReactSuspense
+ * @typedef {React.lazy} ReactLazy
+ */
+
+/*
+ * @typedef { ReactDOM & {createRoot:ReactCreateRoot}}  ReactDom
+ * @typedef {(a:any) => ({render: (a:React.ReactNode) => void})} ReactCreateRoot
+ */
+
+/**
+ * @typedef {(p:Promise<any>)=>Record<'read',Promise<any>>} WrapPromise
+ */
